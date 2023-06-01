ff4c705e70dac00689910b27f4ea140e77aa18f9
Fathur Walkers <fathurwalkers44@gmail.com>
2023-06-01 07:59:14 -0700

    update

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function soal_1()
+    {
+        $nilai = 20;
+        for ($i = 1; $i <= $nilai; $i++) {
+            if ($i % 6 == 0) {
+                echo "DIGITAL OASIS\n";
+            } else if ($i % 2 == 0) {
+                echo "DI\n";
+            } else if ($i % 3 == 0) {
+                echo "OS\n";
+            } else {
+                echo $i."\n";
+            }
+        }
+        return Inertia::render('Dashboard', [
+            'nilai' => $nilai
+        ]);
+    }
+}
